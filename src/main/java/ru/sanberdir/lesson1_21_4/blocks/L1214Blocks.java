ebeb08d6d6b502fd4f5c810da7ec3f6642125936
Sanberdir <91392232+Sanberdir@users.noreply.github.com>
2026-01-14 13:37:39 +0300

    №4

--- src/main/java/ru/sanberdir/lesson1_21_4/blocks/L1214Blocks.java
+++ src/main/java/ru/sanberdir/lesson1_21_4/blocks/L1214Blocks.java
@@ -14,13 +14,12 @@ public class L1214Blocks {
     public static final DeferredBlock<Block> MY_BETTER_BLOCK = BLOCKS.register(
             "my_better_block",
             registryName -> new Block(BlockBehaviour.Properties.of()
-                    .setId(ResourceKey.create(Registries.BLOCK, registryName)).destroyTime(2.0f).explosionResistance(10.0f).sound(SoundType.GRAVEL)
-                    .lightLevel(state -> 7)
+                    .setId(ResourceKey.create(Registries.BLOCK, registryName)).destroyTime(2.0f).explosionResistance(10.0f).sound(SoundType.BASALT)
             ));
     // Способ 2
     public static final DeferredBlock<Block> MY_BETTER_BLOCK2 = BLOCKS.registerBlock(
             "my_better_block2",
             Block::new,
-            BlockBehaviour.Properties.of().destroyTime(2.0f).explosionResistance(10.0f).sound(SoundType.GRAVEL)
+            BlockBehaviour.Properties.of().destroyTime(2.0f).explosionResistance(10.0f).sound(SoundType.COPPER).requiresCorrectToolForDrops()
     );
 }
